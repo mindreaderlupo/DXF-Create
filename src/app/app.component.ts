508c6c196e05373e5d39f7db616dae377ba4b0a4
mindreaderlupo <93987060+mindreaderlupo@users.noreply.github.com>
2023-08-25 21:47:31 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,26 +20,23 @@ import {
 export class AppComponent {
   dxfString = '';
   dxf = new DxfWriter();
-  
 
-  fieldWidth:number = 994;
-  fieldDepth:number = 400;
-  amountRows:number = 5;
+  fieldWidth: number = 994;
+  fieldDepth: number = 400;
+  amountRows: number = 5;
   amountFields: number = 8;
   rotationAngle: number = 0;
   frameWidth: number = 31;
   aisleWidth: number = 1000;
+  rowPattern: string = 'sdd';
 
-  initializeDxf(){
+  initializeDxf() {
     this.dxf.setUnits(Units.Millimeters);
     this.dxf.tables.addAppId('PE Configurator');
     this.dxf.addLayer('r3000', Colors.Green, 'Continuous');
   }
 
-
-
-  createFieldBlock(width:number, depth:number, name:string){
-    
+  createFieldBlock(width: number, depth: number, name: string) {
     const field = this.dxf.addBlock(name);
     const vertices = [
       {
@@ -59,57 +56,57 @@ export class AppComponent {
       },
     ];
     field.addLWPolyline(vertices);
-    field.setLayerName('r3000'); 
+    field.setLayerName('r3000');
     return field;
   }
-  
 
-  createRowBlock(amountFields:number, name:string){
+  createRowBlock(amountFields: number, name: string) {
     const row = this.dxf.addBlock(name);
-    row.addInsert('Rahmen', point3d(0, 0, 0))
+    row.addInsert('Rahmen', point3d(0, 0, 0));
     for (let i = 0; i < amountFields; i++) {
-      row.addInsert('A', point3d((this.frameWidth*(i+1))+(this.fieldWidth*i),0, 0))
-      row.addInsert('Rahmen', point3d((this.frameWidth*(i+1))+(this.fieldWidth*(i+1)),0, 0))
+      row.addInsert(
+        'A',
+        point3d(this.frameWidth * (i + 1) + this.fieldWidth * i, 0, 0)
+      );
+      row.addInsert(
+        'Rahmen',
+        point3d(this.frameWidth * (i + 1) + this.fieldWidth * (i + 1), 0, 0)
+      );
     }
     return row;
   }
 
+  createSystemBlock(amountRows: number, aisleWidth: number, name: string) {
+    const system = this.dxf.addBlock(name);
+    let pattern = [];
+    for (let i = 0; i < amountRows; i++) {
+      if (this.rowPattern == 'sdds') {
+        if (i == 0 || (i == amountRows - 1 && amountRows % 2 == 0)) {
+          pattern.push('s');
+        } else {
+          pattern.push('d');
+        }
+      }
+    }
+    console.log(pattern);
+    let yMeasure = 0;
+    for (let i = 0; i < amountRows; i++) {
+      system.addInsert('row', point3d(0, yMeasure, 0));
+      yMeasure = yMeasure + this.fieldDepth;
+      if (this.rowPattern == 's') {
+        yMeasure = yMeasure + this.aisleWidth;
+      }
+      if (this.rowPattern[i] == 'd') {
+        yMeasure = yMeasure + 6;
+        system.addInsert('row', point3d(0, yMeasure, 0));
+        yMeasure = yMeasure + this.fieldDepth;
+      }
+    }
+  }
+
   generateDXF() {
-
+    /*
     
-/*
-    
-
-    const fieldA = dxf.addBlock('Shelf-top-view');
-    //fieldA.addCircle(point3d(0, 0, 0), 20);
-    const vertices = [
-      {
-        point: point2d(0, 0),
-      },
-      {
-        point: point2d(996, 0),
-      },
-      {
-        point: point2d(996, 400),
-      },
-      {
-        point: point2d(0, 400),
-      },
-      {
-        point: point2d(0, 0),
-      },
-    ];
-    fieldA.addLWPolyline(vertices);
-
-    const row = dxf.addBlock('row');
-    row.addInsert(fieldA.name, point3d(0,0,0))
-    row.addInsert(fieldA.name, point3d(1000,0,0))
-    row.addInsert(fieldA.name, point3d(2000,0,0))
-    row.addInsert(fieldA.name, point3d(3000,0,0))
-    row.addInsert(fieldA.name, point3d(4000,0,0))
-
-
-
     const myBlock2 = dxf.addBlock('myBlock2');
     myBlock2.setLayerName('r3000'); // Layer zuweisung
     myBlock2.addLine(point3d(-100, 10, 0), point3d(0, 20, 0));
@@ -121,11 +118,15 @@ export class AppComponent {
     dxf.addInsert(row.name, point3d(0, 1400, 0));
     dxf.addInsert(row.name, point3d(0, 1830, 0));
 */
-    this.createFieldBlock(this.fieldWidth, this.fieldDepth, "A").setLayerName('r3000'); 
-    this.createFieldBlock(this.frameWidth, this.fieldDepth, "Rahmen").setLayerName('r3000'); 
-    this.createRowBlock(this.amountFields, "row").setLayerName('r3000'); 
-    
-   this.dxf.addInsert('row', point3d(0,4000, 0),{rotationAngle: this.rotationAngle})
+    this.createFieldBlock(this.fieldWidth, this.fieldDepth, 'A');
+    this.createFieldBlock(this.frameWidth, this.fieldDepth, 'Rahmen');
+    this.createRowBlock(this.amountFields, 'row');
+    this.createSystemBlock(this.amountRows, this.aisleWidth, 'system');
+
+    this.dxf.addInsert('system', point3d(0, 0, 0), {
+      rotationAngle: this.rotationAngle,
+      layerName: 'r3000',
+    });
 
     this.dxfString = this.dxf.stringify();
   }
